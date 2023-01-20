a439a1f36683ad7d135375f0738d7bcbd44a2b12
rishabh.kamath <rshbhk22@gmail.com>
2023-01-20 02:03:20 -0600

    added more functions to generateMarkdown.js

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,14 +1,51 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  function renderLicenseBadge(license) {
+    if (!license) {
+      return "";
+    }
+    const licenseBadge = {
+      "MIT": "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)",
+      "Apache": "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)",
+      "GPL": "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)"
+    }
+    return licenseBadge[license] || "";
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  function renderLicenseLink(license) {
+    if (!license) {
+      return "";
+    }
+    const licenseLink = {
+      "MIT": "https://opensource.org/licenses/MIT",
+      "Apache": "https://opensource.org/licenses/Apache-2.0",
+      "GPL": "https://www.gnu.org/licenses/gpl-3.0"
+    }
+    return licenseLink[license] || "";
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (!license) {
+    return "";
+  }
+  const licenseBadge = renderLicenseBadge(license);
+  const licenseLink = renderLicenseLink(license);
+
+  return `## License
+
+This project is licensed under the ${license} License ${licenseBadge}
+
+For more information about the license, click the link below:
+- [${license} License](${licenseLink})`;
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
